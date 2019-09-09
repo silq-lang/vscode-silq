ff872281190fafe02860c1d799dec0f40807871a
Timon Gehr <timon.gehr@gmx.ch>
2019-09-09 22:03:16 +0200

    Various improvements.

--- src/silqRunner.ts
+++ src/silqRunner.ts
@@ -6,20 +6,31 @@ import ChildProcess = cp.ChildProcess;
 
 import * as vscode from 'vscode';
 import { stringify } from 'querystring';
+import { AssertionError } from 'assert';
 
 const outputChannel = vscode.window.createOutputChannel("Silq Output");
+const historyChannel = vscode.window.createOutputChannel("Silq Output History");
 export default class SilqRunner{
     private diagnosticCollection!: vscode.DiagnosticCollection;
     private outputChannel!: vscode.OutputChannel;
     activate(subscriptions: { dispose(): any; }[]) {
         subscriptions.push(this);
-        this.diagnosticCollection = vscode.languages.createDiagnosticCollection(); 
+        this.diagnosticCollection = vscode.languages.createDiagnosticCollection();
         vscode.workspace.onDidOpenTextDocument(this.checkAll, this, subscriptions);
         vscode.workspace.onDidSaveTextDocument(this.checkAll, this);
         // vscode.workspace.onDidChangeTextDocument(this.checkEvent, this, subscriptions); // TODO
         vscode.workspace.onDidCloseTextDocument((textDocument) => {
                 this.diagnosticCollection.delete(textDocument.uri);
         }, null, subscriptions);
+        // register run command
+        vscode.commands.registerCommand("silq.run",()=>{
+            let editor=vscode.window.activeTextEditor;
+            if(editor!==undefined){
+                if(editor.document.isDirty){
+                    editor.document.save();
+                }else this.perform(editor.document, true);
+            }
+        });
         // type check all open documents
         this.checkAll();
     }
@@ -41,17 +52,43 @@ export default class SilqRunner{
         vscode.window.showInformationMessage(msg);
         console.log(msg);
     }
+    private error(msg: string){
+        vscode.window.showErrorMessage(msg);
+        console.error(msg);
+    }
+    private getBinaryPath():string|undefined{
+        let result=vscode.workspace.getConfiguration("silq").get<string>("binaryPath");
+        if(result!==null) return result;
+        let extension = vscode.extensions.getExtension("eth-sri.vscode-silq");
+        if(extension === undefined) return undefined;
+        let file="silq";
+        switch(process.platform){
+            case "darwin": file="silq-osx"; break;
+            case "win32": file="silq.exe"; break;
+            default: break;
+        }
+        return path.join(extension.extensionPath,"bin",file);
+    }
+    childProcess: cp.ChildProcessWithoutNullStreams|undefined = undefined;
     private perform(textDocument: vscode.TextDocument, doRun: boolean){
-        //this.log("checking: "+textDocument.fileName);
-        let executable = vscode.workspace.getConfiguration("silq").get<string>("binaryPath");
+        let executable = this.getBinaryPath();
         if(executable === null){
-            this.log("Error: unable to read silq.binaryPath setting.")
+            this.error("Error: can't run silq. You may need to set silq.binaryPath.");
             return;
         }
         let args = ['--error-json', textDocument.fileName];
         if(doRun) args.push('--run');
         let options = { cwd: path.dirname(textDocument.fileName) };
+        if(doRun&&this.childProcess){
+            this.childProcess.kill();
+            this.log("Previous silq process killed.");
+        }
         let childProcess = cp.spawn(executable as string, args, options);
+        if(doRun){
+            this.childProcess = childProcess;
+            outputChannel.clear();
+            outputChannel.appendLine("running "+textDocument.fileName+"...");
+        }
         let output = '';
         let diagnostics: vscode.Diagnostic[] = [];
         if(childProcess.pid){
@@ -59,17 +96,21 @@ export default class SilqRunner{
                 output += data;
             });
             childProcess.stderr.on('end', async() => {
-                JSON.parse(output).map((item:any):vscode.Diagnostic|null => {
+                let json=[];
+                try{
+                    json=JSON.parse(output);
+                }catch(e){}
+                json.map((item:any):vscode.Diagnostic|null => {
                     let source = item.source as string;
                     let uri=vscode.Uri.file(source);
-                    if(uri.toString()!=textDocument.uri.toString()) return null; // TODO: ok?
+                    if(uri.toString()!==textDocument.uri.toString()) return null; // TODO: ok?
                     let range = new vscode.Range(item.start.line-1, item.start.column, item.end.line-1, item.end.column);
                     let message = item.message as string;
                     let severity = item.severity === "error" ? vscode.DiagnosticSeverity.Error :
                                    item.severity === "note" ? vscode.DiagnosticSeverity.Hint :
                                    vscode.DiagnosticSeverity.Warning;
-                    let diagnostic = new vscode.Diagnostic(range,message,severity);
-                    diagnostic.relatedInformation = item.relatedInformation.map((ritem:any)=>{
+                        let diagnostic = new vscode.Diagnostic(range,message,severity);
+                        diagnostic.relatedInformation = item.relatedInformation.map((ritem:any)=>{
                         let source = ritem.source as string;
                         let range = new vscode.Range(ritem.start.line-1, ritem.start.column, ritem.end.line-1, ritem.end.column);
                         let message = ritem.message as string;
@@ -78,31 +119,41 @@ export default class SilqRunner{
                     return diagnostic;
                 }).forEach((diagnostic: vscode.Diagnostic|null) => {
                     if(diagnostic === null) return;
-                    outputChannel.clear();
                     diagnostics.push(diagnostic);
                 });
                 this.diagnosticCollection.set(textDocument.uri, diagnostics);
-                if(doRun && diagnostics.length===0){
-                    outputChannel.clear();
-                    outputChannel.appendLine("running..."); // TODO
-                    let first = true;
-                    childProcess.stdout.on('data',(data: Buffer) => {
-                        if(first){
-                            outputChannel.clear();
+                if(doRun){
+                    if(diagnostics.length===0){
+                        outputChannel.show(true);
+                        let first = true;
+                        childProcess.stdout.on('data',(data: Buffer) => {
+                            if(first){
+                                if(doRun) outputChannel.clear();
+                                first = false;
+                            }
+                            outputChannel.append(data.toString());
+                            historyChannel.append(data.toString());
                             outputChannel.show(true);
-                            first = false;
-                        }
-                        outputChannel.append(data.toString());
-                    });
-                    childProcess.stdout.on('end',()=>{
-                        if(first){
-                            outputChannel.clear();
-                            first = false;
-                        }
-                    });
+                        });
+                        childProcess.stdout.on('end',()=>{
+                            if(first){
+                                outputChannel.clear();
+                                first = false;
+                            }
+                            outputChannel.appendLine("\n\nResult for "+textDocument.fileName);
+                            outputChannel.show(true);
+                            this.childProcess=undefined;
+                        });
+                    }else{
+                        outputChannel.clear();
+                        outputChannel.appendLine("Type errors in "+textDocument.fileName+" (see \"problems\" window)");
+                        this.childProcess=undefined;
+                    }
+                }else{
+                    this.childProcess=undefined;
                 }
             });
-        }else this.log("Error: unable to run silq. You may need to set silq.binaryPath.");
+        }else this.error("Error: can't run silq. You may need to set silq.binaryPath.");
     }
     public dispose(): void {
         this.diagnosticCollection.clear();
